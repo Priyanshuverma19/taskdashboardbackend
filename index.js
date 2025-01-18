6f9d050840e84bbff82aa4b87646c6beaf57d166
priyanshu verma <priyanshu.vermaa0@gmail.com>
2025-01-18 08:38:50 +0530

    cors

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL||('*'), 
+    origin: process.env.CLIENT_URL||'*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   },
 });
 
