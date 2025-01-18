f7fa0bf9b808646528cbe394271bcd23be8d62bc
priyanshu verma <priyanshu.vermaa0@gmail.com>
2025-01-18 08:13:57 +0530

    change origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'process.env.CLIENT_URL', 
+    origin: process.env.CLIENT_URL||('*'), 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
